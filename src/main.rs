4a2118f96b76e0a3f238afe5b264f2e3de5d795f
Jan Christian Kaessens <j.kaessens@ikmb.uni-kiel.de>
2019-07-09 10:23:22 +0200

    remove histogram, add heatmap

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,17 @@ use crate::binnedvector::FixedBinnedVector;
 
 mod binnedvector;
 
-const BUFFER_SIZE :usize = 1024*1024;
+const BUFFER_SIZE :usize = 64*1024*1024;
 const BIN_COUNT :usize = 1000;
 const HISTOGRAM_SIZE :f32 = 0.125;
 
+
+const MIN_L :f64 = 0.3;
+const MAX_L :f64 = 1.0;
+/*
+const MIN_L :f64 = 0.0;
+const MAX_L :f64 = 1.0;
+*/
 // Parses a [0..1] coordinate pair from a line of text
 fn parse_line(line: &str, xcol: usize, ycol: usize) -> (f32, f32) {
     let fields: Vec<&str> = line.split_whitespace().collect();
@@ -28,10 +35,67 @@ fn parse_line(line: &str, xcol: usize, ycol: usize) -> (f32, f32) {
     }
 }
 
+fn draw_heatmap(data: Vec<u64>, size: (u32, u32), xdescr: &str, ydescr: &str, target: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let bitmap = BitMapBackend::new(target, size).into_drawing_area();
+    bitmap.fill(&White)?;
+
+    let mut chart = ChartBuilder::on(&bitmap)
+        .margin(10)
+        .x_label_area_size(35)
+        .y_label_area_size(50)
+        .build_ranged(0f32..1f32, 0f32..1f32)?;
+
+    // set up mesh and axis description
+    chart.configure_mesh()
+        .x_desc(xdescr)
+        .y_desc(ydescr)
+        .draw()?;
+
+    let max = (*data.iter().max().unwrap() as f64).log10();
+    // draw points vom data vector
+    chart.draw_series(data.into_iter().enumerate().filter_map(|(idx, count)| {
+        if count > 0 {
+            let y = (idx as f32 / size.0 as f32) as u32;
+            let x = (idx as f32 - (y * size.0) as f32) as u32;
+
+            let x_pos = x as f32 / size.0 as f32;
+            let y_pos = y as f32 / size.1 as f32;
+            let lightness = ( ((count as f64).log10() / max as f64) * (MAX_L - MIN_L) + MIN_L);
+            let color = &HSLColor( 0.0/360.0, 1.0, lightness);
+            Some(Circle::new((x_pos,y_pos), 3, color.filled()))
+        } else {
+            None
+        }
+    }))?;
+
+    Ok(())
+}
+
+fn load_data(source: &str, xcol: usize, ycol: usize, size: (u32, u32)) -> Result<Vec<u64>, Box<dyn std::error::Error>> {
+    let mut v = vec![0u64; size.0 as usize * size.1 as usize];
+
+    let bin_x_size = 1f32 / (size.0-1) as f32;
+    let bin_y_size = 1f32 / (size.1-1) as f32;
+
+    let reader = BufReader::with_capacity(BUFFER_SIZE,File::open(source)?);
+
+    for line in reader.lines().skip(1) {
+        let pair = parse_line(&(line.unwrap()), xcol, ycol);
+        let x_bin = (pair.0 / bin_x_size) as usize;
+        let y_bin = (pair.1 / bin_y_size) as usize;
+        v[y_bin * size.1 as usize + x_bin] += 1;
+    }
+
+
+
+    Ok(v)
+}
+
 fn draw_plot(source: &str,
              xcol: usize, ycol: usize,
              xdescr: &str, ydescr: &str,
              target: &str, size: (u32, u32)) -> Result<(), Box<dyn std::error::Error>> {
+
 
     let reader = BufReader::with_capacity(BUFFER_SIZE,File::open(source)?);
 
@@ -59,7 +123,7 @@ fn draw_plot(source: &str,
     chart.configure_mesh()
         .x_desc(xdescr)
         .y_desc(ydescr)
-        .draw()?;
+        .draw().unwrap();
 
     // Read file line-by-line, skip the first one
     let iter = reader.lines().skip(1);
@@ -148,6 +212,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .help("Size in pixels (Y axis)"))
         .get_matches();
 
+    /*
     // run plotter
     draw_plot(matches.value_of("source").unwrap(),
               matches.value_of("x").unwrap_or("6").parse::<usize>().unwrap(),
@@ -157,6 +222,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
               matches.value_of("target").unwrap(),
               (
                   matches.value_of("xsize").unwrap_or("800").parse::<u32>().unwrap(),
-                  matches.value_of("xsize").unwrap_or("800").parse::<u32>().unwrap()
+                  matches.value_of("ysize").unwrap_or("800").parse::<u32>().unwrap()
               ))
+        */
+
+    let size = (
+        matches.value_of("xsize").unwrap_or("800").parse::<u32>().unwrap(),
+        matches.value_of("ysize").unwrap_or("800").parse::<u32>().unwrap()
+    );
+
+    let data = load_data(
+        matches.value_of("source").unwrap(),
+         matches.value_of("x").unwrap_or("6").parse::<usize>().unwrap(),
+         matches.value_of("y").unwrap_or("7").parse::<usize>().unwrap(),
+        size)?;
+
+    draw_heatmap(data,
+                 size,
+                 matches.value_of("xdesc").unwrap_or("Z0"),
+                 matches.value_of("ydesc").unwrap_or("Z1"),
+                 matches.value_of("target").unwrap()
+    )
+
+/*
+    // set up matrix
+    let mut matrix = Array2::<u64>::zeros(size);
+*/
 }
